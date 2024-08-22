ef79787efcf3872b78ae44d34616e7a597d69093
isaigarciamoso <ing.isaimosso@gmail.com>
2024-08-22 08:40:31 -0600

    Movie list feature improvements

--- app/src/main/java/com/mosso/testglobal/movies/presentation/screen/MainActivity.kt
+++ app/src/main/java/com/mosso/testglobal/movies/presentation/screen/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mosso.testglobal.movies.presentation.viewmodel.MovieListViewModel
 import com.mosso.testglobal.ui.theme.TestGlobalTheme
@@ -23,6 +26,7 @@ class MainActivity : ComponentActivity() {
                 val state = viewModel.movieUIState.collectAsStateWithLifecycle().value
                 val lazyListState = rememberLazyListState()
                 MovieListScreen(
+                    modifier = Modifier.padding(top = 32.dp, bottom = 8.dp),
                     uiState = state,
                     lazyListState = lazyListState,
                 )
